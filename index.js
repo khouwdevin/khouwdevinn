aebe9cfcabced4f96a61cf3f3e979f2d280aadc4
Khouw Devin Nathanael <khouwdevin@gmail.com>
2021-10-25 20:39:43 +0700

    init animation

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ var span = document.getElementById("age")
 var date =  new Date().getFullYear()
 var age = date - 2000
 span.textContent = age
+
+function onhover(name, image) {
+    image.style.height = image.style.height * 2
+    image.style.width = image.style.width * 2
+    let texthover = document.getElementById("texthover")
+    texthover.textContent = name
+}
+
+function offhover(image) {
+    image.style.height = image.style.height / 2
+    image.style.width = image.style.width / 2
+    let texthover = document.getElementById("texthover")
+    texthover.textContent = ""
+}
