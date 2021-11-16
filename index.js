27adf1e76f4b0be17a213958119ce4bbb4db2790
Khouw Devin Nathanael <khouwdevin@gmail.com>
2021-11-16 10:53:57 +0700

    add bootstrap

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var span = document.getElementById("age")
-var date =  new Date().getFullYear()
-var age = date - 2000
-span.textContent = age
+const age = () =>{
+    var span = document.getElementById("age")
+    var date =  new Date().getFullYear()
+    var age = date - 2000
+    span.textContent = age
+}
+
+age()
